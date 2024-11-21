fb213b002ba51d5b6772330af03a5091b34884ac
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-20 22:02:29 -0700

    refactor: reset modules

--- xmake.lua
+++ xmake.lua
@@ -41,8 +41,8 @@ target("skse-no-po3tweaks-spam")
     })
 
     -- add source files
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
+    add_files("src/**.cpp")
+    add_headerfiles("src/**.h")
     add_includedirs("src")
     set_pcxxheader("src/PCH.h")
 
